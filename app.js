017ac7d447a8093cfb52018fca60294b424bf2e7
AScodes12 <arransingh99@gmail.com>
2026-08-18 18:37:18 -0700

    Update API_URL to new endpoint

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://fluid-mail-municipal-harbor.trycloudflare.com/api'; 
+const API_URL = 'https://review-baghdad-est-engagement.trycloudflare.com'; 
 
 async function loadPosts() {
     try {
