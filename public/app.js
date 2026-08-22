01a76e1bdb6800d6e22cca190e774cbc75fb1cd5
AScodes12 <arransingh99@gmail.com>
2026-08-22 14:52:38 -0700

    Update views and likes parsing for posts

--- public/app.js
+++ public/app.js
@@ -48,8 +48,9 @@ function switchTab(tabName) {
 function renderPostCard(post) {
     const postId = post.id;
     
-    const views = Number(post.views ?? post.view_count ?? 0);
-    const likes = Number(post.likes ?? post.like_count ?? 0);
+    // SAFE PARSER: If views/likes are arrays from Supabase, get their length! Otherwise parse as number.
+    const views = Array.isArray(post.views) ? post.views.length : (Number(post.views) || 0);
+    const likes = Array.isArray(post.likes) ? post.likes.length : (Number(post.likes) || 0);
     const comments = Array.isArray(post.comments) ? post.comments : [];
 
     const eyeIcon = '<svg class="icon" viewBox="0 0 24 24"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/></svg>';
